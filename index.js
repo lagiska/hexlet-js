3854d4bbc81f1c6730ab106a373e6dd7d3634aa7
lagiska <lagiska@gmail.com>
2024-06-04 18:38:48 +0300

    alter main

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-import _ from "lodash";
-console.log("Мой первый проект на Хекслете");
-console.log(_.last(["one", "two"]));
+import func from "./src/index.js";
+func();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+const func = () => {
+  console.log("Мой первый проект на Хекслете");
+  console.log(_.last(["one", "two"]));
+};
+export default func;
